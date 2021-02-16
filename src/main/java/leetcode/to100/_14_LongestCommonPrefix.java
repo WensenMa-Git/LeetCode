56d60d38c69c2c93a6b34404fb27091b48447fa3
Wensen Ma <wensenma@Wensens-MacBook-Pro.local>
2021-02-15 16:33:48 -0800

    Update Code

--- src/main/java/leetcode/to100/_14_LongestCommonPrefix.java
+++ src/main/java/leetcode/to100/_14_LongestCommonPrefix.java
@@ -23,4 +23,32 @@ public class _14_LongestCommonPrefix {
 
         return strs[0];
     }
+
+    //Provide a second solution.
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String result = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(result) != 0) {
+                result = result.substring(0, result.length() - 1);
+            }
+        }
+        return result;
+    }
+
+    //Provide a third solution.
+    public String longestCommonPrefix3(String[] strs) {
+        if (strs.length == 0) return "";
+        StringBuilder sb = new StringBuilder();
+        String head = strs[0];
+        for (int i = 0; i < head.length(); i++) {
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || head.charAt(i) != strs[j].charAt(i)) {
+                    return sb.toString();
+                }
+            }
+            sb.append(head.charAt(i));
+        }
+        return head;
+    }
 }
