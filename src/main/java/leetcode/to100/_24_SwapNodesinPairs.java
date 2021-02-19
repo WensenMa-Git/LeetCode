a16966eec8b1768d6d3163a1c425061154809a67
Wensen Ma <wensenma@Wensens-MacBook-Pro.local>
2021-02-18 21:52:34 -0800

    Update Code

--- src/main/java/leetcode/to100/_24_SwapNodesinPairs.java
+++ src/main/java/leetcode/to100/_24_SwapNodesinPairs.java
@@ -3,6 +3,11 @@ package leetcode.to100;
 
 import leetcode.dependency.ListNode;
 
+/**
+ * https://leetcode.com/problems/swap-nodes-in-pairs/
+ * Subject: LinkedList
+ * #Medium
+ */
 public class _24_SwapNodesinPairs {
 
     public ListNode swapPairs(ListNode head) {
@@ -20,5 +25,22 @@ public class _24_SwapNodesinPairs {
             l2 = l2.next;
         }
         return dummy.next;
+    }
+
+    //Provide a second solution (Preferred)
+    public ListNode swapPairs2(ListNode head) {
+        ListNode dummyhead = new ListNode(0);
+        dummyhead.next = head;
+        ListNode p = dummyhead;
+        while(p.next != null && p.next.next != null) {
+            ListNode p1 = p.next;
+            ListNode p2 = p1.next;
+            ListNode p3 = p2.next;
+            p2.next = p1;
+            p1.next = p3;
+            p.next = p2;
+            p = p1;
+        }
+        return dummyhead.next;
     }
 }
--- src/main/java/leetcode/to100/_25_ReverseNodesinkGroup.java
+++ src/main/java/leetcode/to100/_25_ReverseNodesinkGroup.java
@@ -1,8 +1,12 @@
 package leetcode.to100;
 
-
 import leetcode.dependency.ListNode;
 
+/**
+ * https://leetcode.com/problems/reverse-nodes-in-k-group/
+ * Subject: LinkedList
+ * #Hard
+ */
 public class _25_ReverseNodesinkGroup {
 
     public ListNode reverseKGroup(ListNode head, int k) {
@@ -24,5 +28,32 @@ public class _25_ReverseNodesinkGroup {
             head = cur;
         }
         return head;
+    }
+
+    //Provide a second solution (Preferred)
+    public ListNode reverseKGroup2(ListNode head, int k) {
+        ListNode dummyhead = new ListNode(0);
+        ListNode p = dummyhead;
+        dummyhead.next = head;
+        while(true) {
+            ListNode p1 = head;
+            for (int i = 0; i < k; i++) {
+                if (head == null) {
+                    return dummyhead.next;
+                }
+                head = head.next;
+            }
+            ListNode pre = null;
+            ListNode p2 = p1;
+            while(p2 != head) {
+                ListNode temp = p2.next;
+                p2.next = pre;
+                pre = p2;
+                p2 = temp;
+            }
+            p1.next = head;
+            p.next = pre;
+            p = p1;
+        }
     }
 }
