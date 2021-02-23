c9a9005845efbbf3dd594e95875ef9adceade365
Wensen Ma <wensen_ma@hotmail.com>
2021-02-23 01:43:18 -0800

    Update Code

--- src/main/java/leetcode/to100/_58_LengthOfLastWord.java
+++ src/main/java/leetcode/to100/_58_LengthOfLastWord.java
@@ -2,6 +2,8 @@ package leetcode.to100;
 
 /**
  * https://leetcode.com/problems/length-of-last-word/
+ * Subject: String
+ * #Easy
  */
 public class _58_LengthOfLastWord {
 
@@ -11,5 +13,19 @@ public class _58_LengthOfLastWord {
             return 0;
         }
         return strings[strings.length - 1].length();
+    }
+
+    public int lengthOfLastWord2(String s) {
+        String[] strings = s.split(" ");
+        if (strings.length == 0) {
+            return 0;
+        }
+        return strings[strings.length - 1].length();
+    }
+
+    //Provide a second solution (Preferred)
+    public int lengthOfLastWord3(String s) {
+        s = s.trim();
+        return s.length() - s.lastIndexOf(' ') - 1;
     }
 }
