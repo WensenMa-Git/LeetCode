444df945df2111cedc5290ce1498a00d6ce47d30
Wensen Ma <wensen_ma@hotmail.com>
2021-03-04 18:09:01 -0800

    Update Code

--- src/main/java/leetcode/to200/_128_LongestConsecutiveSequence.java
+++ src/main/java/leetcode/to200/_128_LongestConsecutiveSequence.java
@@ -1,8 +1,13 @@
 package leetcode.to200;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
-
+/**
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ * Subject: Array
+ * #Hard
+ */
 public class _128_LongestConsecutiveSequence {
 
     public int longestConsecutive(int[] nums) {
@@ -24,6 +29,30 @@ public class _128_LongestConsecutiveSequence {
                 up++;
             }
             res = Math.max(res, up - down - 1);
+        }
+        return res;
+    }
+
+    //Provide a second solution (Preferred).
+    public int longestConsecutive2(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        Arrays.sort(nums);
+        int res = 0;
+
+        int index = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[index++] = nums[i];
+            }
+        }
+
+        for (int L = 0, R = 1; R <= index; R++) {
+            if (R == index || nums[R] != nums[R - 1] + 1) {
+                if (R - L > res) {
+                    res = R - L;
+                }
+                L = R;
+            }
         }
         return res;
     }
--- src/main/java/leetcode/to200/_129_SumRoottoLeafNumbers.java
+++ src/main/java/leetcode/to200/_129_SumRoottoLeafNumbers.java
@@ -2,8 +2,12 @@ package leetcode.to200;
 
 import leetcode.dependency.TreeNode;
 
+/**
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers/
+ * Subject: Backtrack
+ * #Medium
+ */
 public class _129_SumRoottoLeafNumbers {
-
 
     public int sumNumbers(TreeNode root) {
         return helper(root, 0);
@@ -16,5 +20,28 @@ public class _129_SumRoottoLeafNumbers {
         }
         return helper(root.left, num * 10 + root.val) +
                 helper(root.right, num * 10 + root.val);
+    }
+
+    //Provide a second solution (Preferred)
+    public int sumNumbers2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int[] res = new int[]{0, 0};
+        backtrack(res, root);
+        return res[0];
+    }
+
+    private void backtrack(int[] res, TreeNode root) {
+        if (root == null) return;
+        if (root.left == null && root.right == null) {
+            res[0] += (res[1] * 10 + root.val);
+            return;
+        }
+
+        res[1] = res[1] * 10 + root.val;
+        backtrack(res, root.left);
+        backtrack(res, root.right);
+        res[1] /= 10;
     }
 }
