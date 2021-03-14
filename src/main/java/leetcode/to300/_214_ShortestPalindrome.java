42fc4f85b3dd57d56948525c377a0390b95cc5d1
Wensen Ma <wensen_ma@hotmail.com>
2021-03-14 01:09:51 -0800

    Update Code

--- src/main/java/leetcode/to300/_214_ShortestPalindrome.java
+++ src/main/java/leetcode/to300/_214_ShortestPalindrome.java
@@ -1,8 +1,9 @@
 package leetcode.to300;
 
 
-public class _214_ShortestPalindrome {
+import java.util.Map;
 
+public class _214_ShortestPalindrome {
 
     // time :  O(n^2) for aaaaabcaaaaa
     public String shortestPalindrome(String s) {
@@ -23,5 +24,32 @@ public class _214_ShortestPalindrome {
 
     //Provide a second solution
     //https://leetcode-cn.com/problems/shortest-palindrome/solution/shi-jian-fu-za-du-on-jie-fa-la-che-by-time-limit/
-    //
+//    public String shortestPalindrome2(String s) {
+//        int pos = 0, len = s.length();
+//        for (int i = 0; i < s.length(); i++) {
+//            int left = 0, right = i;
+//            while(0 <= 1 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+//                left--;
+//                right++;
+//            }
+//            if (right - left - 1 > len) {
+//                len = right - left - 1;
+//                pos = left + 1;
+//            }
+//            left = i;
+//            right = i + 1;
+//            while(0 <= 1 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+//                left--;
+//                right++;
+//            }
+//            if (left - right - 1 > len) {
+//                len = right - left - 1;
+//                pos = left + 1;
+//            }
+//
+//        }
+//        return s.substring(pos, len);
+//    }
+
+
 }
