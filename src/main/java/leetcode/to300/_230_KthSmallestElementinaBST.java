d6ce6ccd64899091bf02113e2982b03ace4ead33
Wensen Ma <wensen_ma@hotmail.com>
2021-03-30 00:30:09 -0700

    Update Code

--- src/main/java/leetcode/to300/_230_KthSmallestElementinaBST.java
+++ src/main/java/leetcode/to300/_230_KthSmallestElementinaBST.java
@@ -2,6 +2,13 @@ package leetcode.to300;
 
 import leetcode.dependency.TreeNode;
 
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+ * Subject: Tree traversal
+ * #Medium
+ */
 public class _230_KthSmallestElementinaBST {
 
 
@@ -22,5 +29,22 @@ public class _230_KthSmallestElementinaBST {
             res = root.val;
         }
         helper(root.right);
+    }
+
+    //Provide a second solution (Preferred)
+    public static int kthSmallest2(TreeNode root, int k) {
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()) {
+            while(cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            if (--k == 0) return cur.val;
+            cur = cur.right;
+        }
+        return -1;
     }
 }
