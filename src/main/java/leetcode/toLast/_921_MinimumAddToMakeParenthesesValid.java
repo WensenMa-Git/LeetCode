f952d337d71e230d59e40cbe090b8527f6b31647
Wensen Ma <wensen_ma@hotmail.com>
2021-04-07 17:32:40 -0700

    Update Code

---
+++ src/main/java/leetcode/toLast/_921_MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,31 @@
+package leetcode.toLast;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+ * Subject: Stack
+ * #Medium
+ * Similar questions: https://leetcode.com/discuss/interview-question/124551/balance-parenthesis-in-an-alphanumeric-string
+ */
+public class _921_MinimumAddToMakeParenthesesValid {
+
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int result = 0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='('){
+                stack.push(ch);
+            } else{
+                if(stack.isEmpty()) result++;
+                else stack.pop();
+            }
+        }
+        if(!stack.isEmpty()){
+            result+=stack.size();
+        }
+        return result;
+    }
+
+}
