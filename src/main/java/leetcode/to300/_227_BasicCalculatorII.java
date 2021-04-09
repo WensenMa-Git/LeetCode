3080263456019461f530fb81eed66bbb2f13d239
Wensen Ma <wensen_ma@hotmail.com>
2021-04-09 13:55:38 -0700

    Update Code

--- src/main/java/leetcode/to300/_227_BasicCalculatorII.java
+++ src/main/java/leetcode/to300/_227_BasicCalculatorII.java
@@ -9,7 +9,6 @@ import java.util.Stack;
  */
 public class _227_BasicCalculatorII {
 
-    //Preferred Solution.
     public int calculate(String s) {
         if (s == null || s.length() == 0) return 0;
         Stack<Integer> stack = new Stack<>();
@@ -39,8 +38,48 @@ public class _227_BasicCalculatorII {
         return res;
     }
 
+    //Provide a second solution (Preferred Solution).
+    public int calculate2(String s) {
+        if (s == null || s.length() == 0) return 0;
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+        char sign = '+';
+        int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = s.charAt(i) - '0';
+                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    num = num * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                if (sign == '+') stack.push(num);
+                if (sign == '-') stack.push(-num);
+                if (sign == '*') stack.push(stack.pop() * num);
+                if (sign == '/') stack.push(stack.pop() / num);
+            } else if (s.charAt(i) != ' ') {
+                sign = s.charAt(i);
+            }
+        }
+        for (int i : stack) {
+            res += i;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        new _227_BasicCalculatorII().calculate("23+34+34");
+    }
+
+
+
+
+
+
+
+
+
     //time : O(n)  space : O(n)
-    public int calculate1(String s) {
+    public int calculate3(String s) {
         if (s == null || s.length() == 0) return 0;
         Stack<Integer> stack = new Stack<>();
         int res = 0;
@@ -70,7 +109,7 @@ public class _227_BasicCalculatorII {
     }
 
     // time : O(n)  space : O(1)
-    public int calculate2(String s) {
+    public int calculate4(String s) {
         if (s == null || s.length() == 0) return 0;
         s = s.trim().replaceAll(" +", "");
         int res = 0;
