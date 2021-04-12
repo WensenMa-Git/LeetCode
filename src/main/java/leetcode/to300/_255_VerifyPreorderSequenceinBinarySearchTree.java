d6181cc1e3e198e1687e88f5b5e28581f12d2154
Wensen Ma <wensen_ma@hotmail.com>
2021-04-12 16:32:34 -0700

    Update Code

--- src/main/java/leetcode/to300/_255_VerifyPreorderSequenceinBinarySearchTree.java
+++ src/main/java/leetcode/to300/_255_VerifyPreorderSequenceinBinarySearchTree.java
@@ -2,7 +2,11 @@ package leetcode.to300;
 
 import java.util.Stack;
 
-
+/**
+ * https://leetcode.com/problems/verify-preorder-sequence-in-binary-search-tree/
+ * Subject: Tree
+ * #Medium #Attention
+ */
 public class _255_VerifyPreorderSequenceinBinarySearchTree {
 
     public static boolean verifyPreorder(int[] preorder) {
@@ -18,5 +22,19 @@ public class _255_VerifyPreorderSequenceinBinarySearchTree {
             stack.push(num);
         }
         return true;
+    }
+
+    //Provide a second solution (Preferred)
+    int index = 0;
+    public boolean verifyPreorder2(int[] preorder) {
+        recurse(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        return index == preorder.length;
+    }
+
+    public void recurse(int[] preorder, int lower, int upper){
+        if(index >= preorder.length || preorder[index] < lower || preorder[index] > upper){return;}
+        int val = preorder[index++];
+        recurse(preorder, lower, val); // left
+        recurse(preorder, val, upper); // right
     }
 }
