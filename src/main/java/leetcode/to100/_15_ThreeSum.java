7a8b14c1e285629ca95c05e44ec63d84499b7b88
Wensen Ma <wensen_ma@hotmail.com>
2021-04-27 22:22:41 -0700

    Update Code

--- src/main/java/leetcode/to100/_15_ThreeSum.java
+++ src/main/java/leetcode/to100/_15_ThreeSum.java
@@ -44,7 +44,7 @@ public class _15_ThreeSum {
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int target = 0 - nums[i];
+            int target = -nums[i];
             int left = i + 1;
             int right = nums.length - 1;
             while (left < right) {
