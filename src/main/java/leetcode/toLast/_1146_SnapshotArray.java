60b56bbf532179c348e39ad9be40981a571343e7
Wensen Ma <wensen_ma@hotmail.com>
2021-05-05 21:12:34 -0700

    Update Code

---
+++ src/main/java/leetcode/toLast/_1146_SnapshotArray.java
@@ -0,0 +1,29 @@
+package leetcode.toLast;
+
+import java.util.TreeMap;
+
+public class _1146_SnapshotArray {
+
+    private TreeMap<Integer, Integer>[] A;
+    private int snapId = 0;
+    public _1146_SnapshotArray(int length) {
+        A = new TreeMap[length];
+        for (int i = 0; i < length; ++i) {
+            A[i] = new TreeMap<Integer, Integer>();
+            A[i].put(0, 0);
+        }
+    }
+
+    public void set(int index, int val) {
+        A[index].put(snapId, val);
+    }
+
+    public int snap() {
+        return snapId++;
+    }
+
+    public int get(int index, int snap_id) {
+        return A[index].floorEntry(snap_id).getValue();
+    }
+
+}
